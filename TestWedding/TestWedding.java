17ac8d51171f5d0e2f4089062c589596fc606a0e
felixalbores <felixyosoresalbores@gmail.com>
2021-03-18 22:28:42 +0800

    Zodiac

--- TestWedding/TestWedding.java
+++ TestWedding/TestWedding.java
@@ -1,11 +1,3 @@
-/* Create a class named Person that holds the following fields: two String objects for the person’s first and
-last name and a LocalDate object for the person’s birthdate. Create a class named Couple that contains
-two Person objects. Create a class named Wedding for a wedding planner that includes the date of the
-wedding, the names of the Couple being married, and a String for the location. Provide constructors for
-each class that accept parameters for each field, and provide get methods for each of the field. Then
-write a program that creates two Wedding objects and in turn passes each to a method that displays all
-the details. Save the files as Person.java, Couple.java, Wedding,java, and TestWedding.java.
-*/
 public class TestWedding{
    public static void main(String args[]){
       Person male = new Person("Sakura","Haruno",2000,12,26);
---
+++ Zodiac Sign/Person.java
@@ -0,0 +1,45 @@
+/* Create a class Person with the following attributes:
+-	lastname : String
+-	firstname: String
+-	gender: char
+-  birthdate: Birthdate
+Create the appropriate constructors, setters and getters
+Create 3 objects of the Person class*/
+import java.time.LocalDate; 
+import java.time.Period;
+public class Person{
+   String lastName;
+   String firstName;
+   char gender;
+   LocalDate  birthDate;
+
+   public Person(String lastName, String firstName, char gender,int year,int month,int day){
+      this.lastName = lastName;
+      this.firstName = firstName;
+      this.gender = gender;
+      birthDate = LocalDate.of(year,month,day);
+   }
+
+
+ public int getAge(){
+   LocalDate today = LocalDate.now();
+   int age = Period.between(birthDate,today).getYears();
+   return age;
+ }
+ /*
+ public String getZodiac(){
+   March 21 - April 19 - Aries -- April 19 
+ 
+ //return the zodiac sign of a person based on his birthdate.
+ //search the zodiac signs online.
+ }
+ */
+   public static void main(String args[]){
+      Person p1 = new Person("Albores","Felix",'M',2000,12,26);
+      int a = p1.birthDate.getDayOfMonth();//26
+      System.out.println(p1.birthDate.getMonthValue());
+      System.out.println(a);
+         System.out.print("Aries");
+      
+   }
+}
