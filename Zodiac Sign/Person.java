8475f09e87955e17346f1fd00a9d45aa69a0fe6a
felixalbores <felixyosoresalbores@gmail.com>
2021-03-19 11:43:49 +0800

    Commited nako

--- Zodiac Sign/Person.java
+++ Zodiac Sign/Person.java
@@ -1,4 +1,3 @@
-//SAMpLE
 /* Create a class Person with the following attributes:
 -	lastname : String
 -	firstname: String
@@ -6,41 +5,86 @@
 -  birthdate: Birthdate
 Create the appropriate constructors, setters and getters
 Create 3 objects of the Person class*/
-import java.time.LocalDate; 
+//Felix Albores 
+import java.time.LocalDate;
 import java.time.Period;
 public class Person{
-   String lastName;
-   String firstName;
-   char gender;
-   LocalDate  birthDate;
+    String lastName;
+    String firstName;
+    char gender;
+    LocalDate  birthDate;
 
-   public Person(String lastName, String firstName, char gender,int year,int month,int day){
-      this.lastName = lastName;
-      this.firstName = firstName;
-      this.gender = gender;
-      birthDate = LocalDate.of(year,month,day);
-   }
+    public Person(String lastName, String firstName, char gender,int year,int month,int day){
+        this.lastName = lastName;
+        this.firstName = firstName;
+        this.gender = gender;
+        birthDate = LocalDate.of(year,month,day);
+    }
+    //setters
+    public void setLastName(String lastName){this.lastName = lastName;}
+    public void setFirstName(String firstName){this.firstName = firstName;}
+    public void setGender(char gender){this.gender = gender;}
+    public void setBirthDate(int year,int month,int day){birthDate = LocalDate.of(year,month,day);}
+    
+    //getters
+    public String getLastName(){return lastName;}
+    public String getFirstName(){return firstName;}
+    public char getGender(){return gender;}
+    public LocalDate getBirthDate(){return birthDate;}    
 
+    public int getAge(){
+        LocalDate today = LocalDate.now();
+        int age = Period.between(birthDate,today).getYears();
 
- public int getAge(){
-   LocalDate today = LocalDate.now();
-   int age = Period.between(birthDate,today).getYears();
-   return age;
- }
- /*
- public String getZodiac(){
-   March 21 - April 19 - Aries -- April 19 
- 
- //return the zodiac sign of a person based on his birthdate.
- //search the zodiac signs online.
- }
- */
-   public static void main(String args[]){
-      Person p1 = new Person("Albores","Felix",'M',2000,12,26);
-      int a = p1.birthDate.getDayOfMonth();//26
-      System.out.println(p1.birthDate.getMonthValue());
-      System.out.println(a);
-         System.out.print("Aries");
-      
-   }
+        return age;
+    }
+    
+    public String getZodiac(){
+        
+        byte month = (byte)birthDate.getMonthValue();
+        byte day = (byte)birthDate.getDayOfMonth();
+        
+        if(month== 1 && day>=20 || month == 2 && day<=18)
+            return "Aquarius";
+        else if(month== 2 && day>=19 || month == 3 && day<=20)
+            return "Pisces";
+        else if(month == 3 && day>=21 || month == 4 && day<=19)
+            return "Aries";
+        else if(month == 4 && day>=20 || month == 5 && day<=20)
+            return "Taurus";
+        else if(month == 5 && day>=21 || month== 6 && day<=21)
+            return "Gemini";
+        else if(month == 6 && day>=22 || month== 7 && day<=22)
+            return "Cancer";
+        else if(month == 7 && day>=23 || month == 8 && day<=22)
+            return "Leo";
+        else if(month == 8 && day>=23 || month == 9 && day<=22)
+            return "Virgo";
+        else if(month == 9 && day>=23 || month  == 10 && day<=23)
+            return "Libra";
+        else if(month == 10  && day>=24 || month  == 11 && day<=21)
+            return "Scorpio";
+        else if(month == 11  && day>=22 || month  == 12 && day<=21)
+            return "Sagittarius";
+        else
+            return "Capricorn";
+    }
+    public void displayInformation(){
+      System.out.println("Person Information "
+                        +"\n\nLastName: " + getLastName()
+                        +"\nFirstName: " + getFirstName()
+                        +"\nBirtDate: " + birthDate
+                        +"\nZodiac Sign: " + getZodiac());
+    }
+    public static void main(String args[]){
+                                                   //yyyy//mm/dd
+        Person p1 = new Person("Albores","Felix",'M',2000,1,20);
+        p1.displayInformation();
+         
+        Person p2 = new Person("Ackerman","Mikasa",'F',1998,2,23);
+        p2.displayInformation();
+        
+        Person p3 = new Person("Yeager","Eren",'M',2004,11,1);        
+         p2.displayInformation();
+    }
 }
