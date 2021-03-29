d55a3919191556021b16036a1ee938ce40c3d9f9
felixalbores <felixyosoresalbores@gmail.com>
2021-03-29 22:36:02 +0800

    Problem and Solution

--- PigLatin/PigLatin.java
+++ PigLatin/PigLatin.java
@@ -1,7 +1,13 @@
 /* 
    Write an application that accepts a word from a user and converts it to Pig Latin.
    If a word starts with a consonant, the Pig Latin version removes all consonants from the beginning of the word and places them at the end, followed by ay.
-   For example, cricket becomes icketcray.If a word starts with a vowel,the Pig Latin version is the original word with ay added to the end. For example, apple becomes appleay. If y is the  first letter in a word, it is treated as a consonant; otherwise, it is treated as a vowel. For example, young becomes oungyay, but system becomes ystemsay. For this program, assume that the user will enter only a single word consisting of all lowercase letters. Save the file as PigLatin.java.
+   For example, cricket becomes icketcray.If a word starts with a vowel,
+   the Pig Latin version is the original word with ay added to the end.
+   For example, apple becomes appleay. If y is the  first letter in a word,
+   it is treated as a consonant; otherwise, it is treated as a vowel.
+   For example, young becomes oungyay, but system becomes ystemsay. For this program, 
+   assume that the user will enter only a single word consisting of all lowercase letters. 
+   Save the file as PigLatin.java.
 */
 //Felix Albores
 import java.util.Scanner;
@@ -28,5 +34,4 @@ public class PigLatin{
               }   
                 System.out.println("PigLatin: " + sb.append("ay"));
    }
-
 }
