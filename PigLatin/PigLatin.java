ce8194f202b249d1d70862deb59b9a98e37e6f59
felixalbores <felixyosoresalbores@gmail.com>
2021-03-29 20:53:28 +0800

    inputing deleteCharAt(0) method

--- PigLatin/PigLatin.java
+++ PigLatin/PigLatin.java
@@ -1,59 +1,32 @@
 import java.util.Scanner;
 public class PigLatin{
          
-       static String pigLatinWord = "ay";
-       static char[] vowels = {'a','e','i','o','u'};
-        
        static Scanner scanner = new Scanner(System.in);
       
       public static void main(String[] args){
-       System.out.print("Input Word: ");
-             String word = scanner.next();
-             
-             
-          StringBuilder sb = new StringBuilder(word);
-                             //f           //elix
-               char letter = sb.charAt(0);                  
-                           sb.deleteCharAt(0);
-                        System.out.println(sb.append(letter));
-                        //sb.append((sb.charAt(0)));
-                         //     System.out.println(sb); 
-             
-//            for(int i = 0;i<sb.length();i++){
-//               switch(sb.charAt(i)){
-//                      case 'a': case 'i': case 'e': case 'u': case 'o':
-//                        break;
-//                        default:
-//                           sb.deleteCharAt(0);
-//                           sb.append(sb.charAt(0));    
-//               }
-//               
-//           }       
-                System.out.println("PigLatin: " + sb.append(pigLatinWord));
+            
+            System.out.print("Input Word: ");        
+            StringBuilder sb = new StringBuilder(scanner.next());
+                          
+         char letter = ' '; //felix = 5
+           for(int i = 0;i<sb.length();i++){
+              switch(sb.charAt(i)){
+                     case 'a': case 'i': case 'e': case 'u': case 'o':
+                       break;
+                       default:
+                      letter = sb.charAt(0);
+                      sb.deleteCharAt(0);
+                      sb.append(letter);  
+              }
+              
+          }       
+                System.out.println("PigLatin: " + sb.append("ay"));
                
             
           
           
           
           
-          
-          
-          
-          
-          //  //            StringBuilder sb = new StringBuilder(word);
-//                 for(int i = 0;i<vowels.length;i++){
-//                    if(vowels[i] == firstLetterOfWord){
-//                      sb.append(pigLatinWord);
-//                      break;
-//                   }else if(i == vowels.length - 1){
-//                              sb.deleteCharAt(0);
-//                              sb.append(firstLetterOfWord);
-//                              sb.append(pigLatinWord);        
-//                        }             
-//                }
-//                System.out.println("Pig Latin: " + sb);
-//                   
-     
 
    }
 
