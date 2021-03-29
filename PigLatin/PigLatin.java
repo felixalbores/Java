4b34db7df1414df904cb7efe322e2fbb5b981ab0
felixalbores <felixyosoresalbores@gmail.com>
2021-03-29 15:15:21 +0800

    Problem

---
+++ PigLatin/PigLatin.java
@@ -0,0 +1,17 @@
+public class PigLatin{
+   
+   public static void main(String[] args[]){
+      
+   }
+   
+   /*
+      if a words start with a constant the first letter word will be removed and place them at the end
+      of the word followed by ay       
+         system--> ystem say
+                                  [a,i,e,o,u]
+         if a words starts with a vowel then the original word are same and the last letter of the word would be
+         added ay
+         apple+ay
+   */
+
+}
