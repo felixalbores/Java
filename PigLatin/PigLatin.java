e6ebb06f3581aed0f0f96aaf559e443744dedd19
felixalbores <felixyosoresalbores@gmail.com>
2021-03-29 15:26:52 +0800

    Sample sa Consonant

--- PigLatin/PigLatin.java
+++ PigLatin/PigLatin.java
@@ -1,10 +1,21 @@
 public class PigLatin{
    
-   public static void main(String[] args[]){
+   public static void main(String[] args){
       
+      char[] vowels = {'a','e','i','o','u'};
+     
+      String word = "Hello";//the first letter is H its consants os the answer will be ellohay
+     
+      System.out.println(vowels[0]);
+     
+//      for(c = 'A'; c <= 'z'; ++c)
+//       System.out.print(c + " ");
+// 
    }
-   
-   /*
+
+}
+
+/*
       if a words start with a constant the first letter word will be removed and place them at the end
       of the word followed by ay       
          system--> ystem say
@@ -12,6 +23,5 @@ public class PigLatin{
          if a words starts with a vowel then the original word are same and the last letter of the word would be
          added ay
          apple+ay
+         
    */
-
-}
