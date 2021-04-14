d6b2bd78acedac947087f73d598f95990c3efdac
felixalbores <felixyosoresalbores@gmail.com>
2021-04-14 23:50:06 +0800

    It change but display might have small problem but the logic is okay

--- ArraysOfObjectRecording/RecordingSort.java
+++ ArraysOfObjectRecording/RecordingSort.java
@@ -3,88 +3,87 @@ import java.util.Scanner;
  public class RecordingSort{
          
         
-      static Scanner scanner = new Scanner(System.in);
+        static Scanner scanner = new Scanner(System.in);
         static Recording[] records = new Recording[3];
-      
-    //Prompt the User Method
-     public static Recording promptUser(){
+       
+          //Prompt the User Method
+          public static Recording promptUser(){
    
-            Recording record = new Recording();
+              Recording record = new Recording();
+             
+              System.out.print("Song Title: ");     
+              record.setTitle(scanner.nextLine());
             
-            System.out.print("Song Title: ");     
-            record.setTitle(scanner.nextLine());
+              System.out.print("Artist: ");
+              record.setArtist(scanner.nextLine());
             
-            //System.out.print("Artist: ");
-            //record.setArtist(scanner.nextLine());
-            
-            //System.out.print("Playing Time in Second: ");
-            //record.setSecond(scanner.nextInt());
+              System.out.print("Playing Time in Second: ");
+              record.setSecond(scanner.nextInt());
       
-           return record;
-   }
+             return record;
+      }
    
-   //Sorting Method
-   public static void sorting(int choose){
-               
-          //Sorting For SongTitle
-          if(choose == 1){
-         	int comparisonToMake = records.length - 1;
-				for (int i = 0; i < comparisonToMake; i++) {
-					for (int j = 0; j < comparisonToMake; j++) {
-						int k  = j + 1;
-							if (records[j].getTitle().compareTo(records[k].getTitle()) > 0) {
-								Recording temp = records[j];
-								records[j] = records[k];
-								records[k] = temp;
-							}	        
-               }     
-            }
-        }//Sorting for Artist
-        else if(choose == 2){
-           int comparisonToMake = records.length - 1;
-				for (int i = 0; i < comparisonToMake; i++) {
-					for (int j = 0; j < comparisonToMake; j++) {
-						int k  = j + 1;
-							if (records[j].getArtist().compareTo(records[k].getArtist()) > 0) {
-								Recording temp = records[j];
-								records[j] = records[k];
-								records[k] = temp;
-							}	        
-               }     
-            }
-        }//Sorting for Playing Time in a Second
-        else if (choose == 3){
-          int comparisonToMake = records.length - 1;
-				for (int i = 0; i < comparisonToMake; i++) {
-					for (int j = 0; j < comparisonToMake; j++) {
-						int k  = j + 1;
-							if (records[j].getSecond().compareTo(records[k].getSecond()) > 0) {
-								Recording temp = records[j];
-								records[j] = records[k];
-								records[k] = temp;
-							}	        
-               }     
-            }
-        }
-        
+        //Sorting Method
+      public static void sorting(int choose){
+                     
+                //Sorting For SongTitle
+                if(choose == 1){
+               	int comparisonToMake = records.length - 1;
+      				for (int i = 0; i < comparisonToMake; i++) {
+      					for (int j = 0; j < comparisonToMake; j++) {
+      						int k  = j + 1;
+      							if (records[j].getTitle().compareTo(records[k].getTitle()) > 0) {
+      								Recording temp = records[j];
+      								records[j] = records[k];
+      								records[k] = temp;
+      							}	        
+                     }     
+                  }
+              }//Sorting for Artist
+              else if(choose == 2){
+                 int comparisonToMake = records.length - 1;
+      				for (int i = 0; i < comparisonToMake; i++) {
+      					for (int j = 0; j < comparisonToMake; j++) {
+      						int k  = j + 1;
+      							if (records[j].getArtist().compareTo(records[k].getArtist()) > 0) {
+      								Recording temp = records[j];
+      								records[j] = records[k];
+      								records[k] = temp;
+      							}	        
+                     }     
+                  }
+              }//Sorting for Playing Time in a Second
+              else if (choose == 3){
+                int comparisonToMake = records.length - 1;
+      				for (int i = 0; i < comparisonToMake; i++) {
+      					for (int j = 0; j < comparisonToMake; j++) {
+      						int k  = j + 1;
+      							if (records[j].getSecond().compareTo(records[k].getSecond()) > 0) {
+      								Recording temp = records[j];
+      								records[j] = records[k];
+      								records[k] = temp;
+      							}	        
+                     }     
+                  }
+              }
+              
  }  
-     //display Method
-       public static void display(){
-            
-               
+          //display Method
+           public static void display(){
+                           
                for(int i = 0;i<records.length;i++){
-                  System.out.println("Song Title: " + records[i].getTitle() + " \tArtist: " + records[i].getArtist() + "\t Playing Time In Second: " + records[i].getSecond()) ;
-               }
-         
-       }
+                    System.out.print("Song Title: " + records[i].getTitle() + "\t\tArtist: " + records[i].getArtist() + "\t\t\tSeconds: " + records[i].getSecond());
+                    System.out.println();
+                }
+          }
    
        public static void main(String args[]){
             
          for(int i = 0;i<records.length;i++){
                   System.out.println("Recording " + (i+1) + " :");
                      records[i] = promptUser();
-                    // scanner.nextLine();
-                  System.out.println();
+                     scanner.nextLine();
+                    System.out.println();
             }
       
       //Choosing  Field which being be sorted  
