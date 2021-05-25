e8f78fa1bf4d2b363dab07cb66d365cf68d4fc0c
felixalbores <felixyosoresalbores@gmail.com>
2021-05-25 17:52:13 +0800

    GUI JCheckBox

---
+++ LastManStandingGUI/LastManStanding.java
@@ -0,0 +1,16 @@
+import javax.swing.JCheckBox;
+public class LastManStanding{
+
+   JCheckBox box_1 = new JCheckBox();
+   JCheckBox box_2 = new JCheckBox();
+   JCheckBox box_3 = new JCheckBox();
+   JCheckBox box_4 = new JCheckBox();
+   JCheckBox box_5 = new JCheckBox();
+   JCheckBox box_6 = new JCheckBox();
+   JCheckBox box_7 = new JCheckBox();
+   JCheckBox box_8 = new JCheckBox();
+   JCheckBox box_9 = new JCheckBox();
+   JCheckBox box_10 = new JCheckBox();
+   
+   
+}
